d54cd1a6ca8fc7b5290db5aeeda248998d54e752
Wladimir Burbano <Ing.SIGECA1@asohofrucol.com.co>
2026-05-15 22:29:49 -0500

    feat: initialize new Ionic Angular application with routing and Firebase integration
    - Added app component with routing setup
    - Created home page component with basic layout
    - Implemented global styles and necessary polyfills
    - Configured Firebase environment settings for production and development
    - Added favicon and SVG assets
    - Set up testing framework for components
    - Defined TypeScript configuration for the application

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAhO9jsgrKuG13k3yhWLAMdKUqDhDJHH3Q',
+    authDomain: 'accenture-franchise.firebaseapp.com',
+    projectId: 'accenture-franchise',
+    storageBucket: 'accenture-franchise.firebasestorage.app',
+    messagingSenderId: '574079770627',
+    appId: '1:574079770627:web:dedc214a7e94696a42ae36',
+  },
+};
